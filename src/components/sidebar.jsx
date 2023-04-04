7f379589e39047a4092e6c6388f956304cc9670c
Lawrence Shen <lawrenceshen1213@gmail.com>
2023-04-05 01:54:01 +1000

    "fixing sidebar"

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
 
-function Sidebar() {
+export default class Sidebar extends Component {
+  render() {
     return (
       <div>
         <div>
           <nav href="#navbar" className="js-colorlib-nav-toggle colorlib-nav-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><i /></nav>
           <aside id="colorlib-aside" className="border js-fullheight">
             <div className="text-center">
-              <div className="author-img" style={{backgroundImage: 'url(images/image-profile.jpg)'}} />
+              <div className="author-img" style={{backgroundImage: 'url(images/about.jpg)'}} />
               <h1 id="colorlib-logo"><a href="index.html">Lawrence Shen</a></h1>
               <span className="email"><i className="icon-mail"></i> lawrenceshen1213@gmail.com</span>
             </div>
@@ -16,16 +17,18 @@ function Sidebar() {
                 <ul>
                   <li className="active"><a href="#home" data-nav-section="home">Introduction</a></li>
                   <li><a href="#about" data-nav-section="about">About</a></li>
-                  <li><a href="#timeline" data-nav-section="timeline">Timeline</a></li>
-                  <li><a href="#blog" data-nav-section="blog">Blog</a></li>
+                  {/*<li><a href="#" data-nav-section="projects">Projects</a></li>
+                  <li><a href="#" data-nav-section="blog">Blog</a></li>*/}
+                  <li><a href="#timeline" data-nav-section="timeline">Experience</a></li>
+                  <li><a href="#" data-nav-section="blog">Blog</a></li>
                 </ul>
               </div>
             </nav>
             <nav id="colorlib-main-menu">
               <ul>
                 <li><a href="https://www.linkedin.com/in/lawrence-shen-9b23791a4/" target="_blank" rel="noopener noreferrer"><i className="icon-linkedin2" /></a></li>
-                <li><a href="https://medium.com/@lawrenceshen1213" target="_blank" rel="noopener noreferrer"><i className="icon-blogger2"></i></a></li>
                 <li><a href="https://github.com/lawrieshen" target="_blank" rel="noopener noreferrer"><i className="icon-github"></i></a></li>
+                <li><a href="https://medium.com/@lawrenceshen1213" target="_blank" rel="noopener noreferrer"><i className="icon-blogger2"></i></a></li>
               </ul>
             </nav>
             <div className="colorlib-footer">
@@ -37,6 +40,5 @@ function Sidebar() {
         </div>
       </div>
     )
+  }
 }
-
-export default Sidebar;
