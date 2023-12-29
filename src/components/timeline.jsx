5b59c41ee256efc648d801b72982673bd1cc462f
Lawrence Shen <lawrenceshen1213@gmail.com>
2023-12-29 23:15:06 +1100

    edit the layout

--- src/components/timeline.jsx
+++ src/components/timeline.jsx
@@ -20,13 +20,15 @@ function Timeline() {
                         <i className="icon-pen2" />
                       </div>
                       <div className="timeline-label">
-                        <h2>Junior Portfolio Project Analyst at Hummingbird Solutions <span>2023/01-present</span></h2>
-                        <img src="images/hummingbird.jpg" style={{width: "90%", height: "auto", paddingBottom: "22.5px", margin: "auto", display: "block"}}/>
+                        <div className='title-box' style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+                          <h2>Junior Portfolio Project Analyst at Hummingbird Solutions</h2> <span>2023/01-present</span>
+                        </div>
+                        <img src="images/hummingbird.jpg" />
                         <p>Hummingbird Solutions is a pioneer startup combining IT services and project management consultancy. As a part of the managed service team, my role is responsible for facilitating clients onboarding project management software and providing high-level PMO insights.</p>
                         <ul>
                          <li>Providing high-level administrative support by coordinating correspondence, preparing statistical reports, handling information requests, and performing PMO work.</li> 
                          <li>Monitoring project costs, and progress against schedule from the Project Management System.</li>
-                         <li>Developing system training materials and supporting the training of Hummingbird clients.</li>
+                         <li>Anayzed requirements for cleint's new Learning Management System.</li>
                         </ul>
                       </div>
                     </div>
@@ -37,9 +39,12 @@ function Timeline() {
                         <i className="icon-pen2" />
                       </div>
                       <div className="timeline-label">
-                        <h2>Finalist at PwC Inter-Uni Case Competition 2022<span>2022/09-2022/10</span></h2>
+                        <div className='title-box'>
+                          <h2>Finalist at PwC Inter-Uni Case Competition 2022</h2><span>2022/09-2022/10</span>
+                        </div>
+                        
                         <a href="https://www.canva.com/design/DAFMGMXkrU4/QDtIONvFEBjbnsLh_oheFA/view?utm_content=DAFMGMXkrU4&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink" className="blog-img" target="_blank">
-                          <img src="images/pwccasecomp.jpg" className="img-responsive" />
+                          <img src="images/pwccasecomp.jpg"/>
                         </a>
                         <p>PwC aims to create sustainable infrastructure for Smart Cities through technology solutions like a smart payment system. Our team proposed JigglePay, a digital wallet with integrated government payment gateway, asset info, and carbon footprint calculator. We also suggested a BI solution for zero-carbon cities using SAP HANA Cloud, hybrid physical-cloud server architecture, and AccessibleJiggle for seniors and disabilities. We plan to partner with Australia's Be Connected Program and use Low-Earth-Orbit Satellite Network for better accessibility. Our solution was presented to PwC’s professional consultants and awarded the finalist.</p>
                         <ul>
@@ -57,7 +62,9 @@ function Timeline() {
                         <i className="icon-pen2" />
                       </div>
                       <div className="timeline-label">
-                        <h2>US Market E-Commerce Intern at BenQ Corporation <span>2020/11-2021/09</span></h2>
+                        <div className='title-box'>
+                          <h2>US Market E-Commerce Intern at BenQ Corporation</h2><span>2020/11-2021/09</span>
+                        </div>
                         <p>BenQ Corporation is a prestigious, multinational consumer electronics company. They have been striving to transform their existing brick-and-mortar business model into an E-commerce business model since 2019. My role in the team is to increase their loyal customers’ adoption to the new E-Commerce website.</p>
                         <ul>
                          <li>Performed inbound marketing with Hubspot, contacting over 50 customers per day with 95% of customer satisfaction rate.</li> 
